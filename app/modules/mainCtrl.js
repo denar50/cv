876d367def39d88e348713da190cee4740cafd0f
Denar <lobotk50@gmail.com>
2015-09-12 21:57:30 -0500

    Skills page almost finished.

--- app/modules/mainCtrl.js
+++ app/modules/mainCtrl.js
@@ -1,8 +1,10 @@
-angular.module('esCv').controller('MainCtrl', [MainCtrl]);
-function MainCtrl(){
+angular.module('esCv').controller('MainCtrl', ['$timeout', MainCtrl]);
+function MainCtrl($timeout){
   var self = this;
   self.sliderUrls = ['assets/img/slider1.jpg', 'assets/img/slider2.jpg', 'assets/img/slider3.jpg'];
-  $(document).ready(initDom);
+  $(document).ready(function(){
+    $timeout(initDom, 0);
+  });
 
   function initDom(){
     $(document).foundation();
@@ -12,7 +14,7 @@ function MainCtrl(){
       $('.menu-item a[href^="#"]').each(function(){
         var hash = this.hash;
         var target = $(hash);
-        var scrollToPosition = target.offset().top - 62;
+        var scrollToPosition = target.offset().top - 60;
         $(this).off('click');
         $(this).on('click', function(e){
           $('html, body').stop().animate({
