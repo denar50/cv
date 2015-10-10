1efb62fcee679fab44cec342f4bbb1e0189679f0
Denar <lobotk50@gmail.com>
2015-10-10 16:00:52 -0500

    The esStickyNav directive has been simplified

--- app/directives/esStickyNav/directive.js
+++ app/directives/esStickyNav/directive.js
@@ -14,8 +14,6 @@ function esStickyNav($timeout){
 				navElementActiveClass: scope.navElementActiveClass || 'active'
 			};
 
-			var initialElementTop;
-
 			var isStuck = false;
 
 			var currentNavElement;
@@ -27,39 +25,29 @@ function esStickyNav($timeout){
 				init();
 			});
 
-
 			function stickToTop(){
-				if(!element.hasClass(classes.stickClass))
-				{
-					element.addClass(classes.stickClass);
-					element.css({
-						position: 'fixed',
-						top: 0
-					});
+				element.addClass(classes.stickClass);
+				element.css({
+					position: 'fixed',
+					top: 0
+				});
 
-					element.parent().css({
-						paddingBottom: elementHeight + 'px'
-					});
-				}
+				element.parent().css({
+					paddingBottom: elementHeight + 'px'
+				});
+				isStuck = true;
 			}
 
 			function unstick(){
 				element.removeClass(classes.stickClass);
 				element.attr('style', '');
 				element.parent().attr('style', '');
+				isStuck = false;
 			}
 
 			function init(){
 				updateNavBar();
-				setElInitialTopPosition();
 				addOnScrollEvent();
-				addOnResizeWindowEvent();
-			}
-
-			function addOnResizeWindowEvent(){
-				$(window).resize(function(){
-					setElInitialTopPosition();
-				});
 			}
 
 			function getCurrentElementContent(){
@@ -85,13 +73,12 @@ function esStickyNav($timeout){
 				$(currentNavElement).addClass(classes.navElementActiveClass);
 			}
 
-			function setElInitialTopPosition(){
-				if($(element).css('position') === 'fixed'){
-					initialElementTop = $(element).parent().height() - elementHeight;
-				}
-				else {
-						initialElementTop = $(element).parent().height();
-				}
+			function getElInitialTopPosition(){
+				var initialElementTop = $(element).parent().height()
+				/*if($(element).css('position') === 'fixed'){
+					 initialElementTop - elementHeight;
+				}*/
+				return initialElementTop;
 			}
 
 			function getDistanceFromTop(el){
@@ -105,12 +92,13 @@ function esStickyNav($timeout){
 				$(document).on('scroll', function(){
 					var distanceFromTop = getDistanceFromTop(element);
 					var scrollTop = $(window).scrollTop();
-					if(distanceFromTop <= 0 && scrollTop >= (initialElementTop-elementHeight)){
-						stickToTop();
+					if(isStuck)
+					{
+						scrollTop < getElInitialTopPosition() && unstick();
 					}
 					else
 					{
-							unstick();
+						distanceFromTop <= 0 && stickToTop();
 					}
 					updateNavBar();
 				});
